34d815720a134ca417be82ce8fae24aeebc5212c
nilay <nilayanand@gmail.com>
2024-01-19 23:27:33 +0530

    Ruby CLI script to fetch todos

---
+++ README.md
@@ -0,0 +1,19 @@
+# RUBY CLI Application 
+
+This simple CLI application prints first 20 even numbered todos when executed. The todos are being fetched from API.
+
+In order to run this CLI app, you need to have ruby interpreter executable under `/usr/bin/env`.
+
+Checkout CLI project, `cd` into project directory and then make the `todos.rb` file executable
+
+`$ chmod +x my_cli_app.rb`
+
+Now you can run CLI application:
+$ ./todos.rb
+
+If you do not have ruby interpreter set under `/usr/bin/env` directory, you can directly run as:
+$ ruby todos.rb
+
+
+result:
+![alt text](https://github.com/nilay/ruby-cli/blob/main/screenshot.jpg?raw=true)
---
+++ todos.rb
@@ -0,0 +1,40 @@
+#!/Users/nilay/.rvm/rubies/ruby-3.3.0/bin/ruby
+require 'net/http'
+require 'open-uri'
+require 'json'
+require 'console_table'   # tabular print in console
+require 'colorize'        # colored print in console
+require 'yajl'            # load json in chunk
+
+TODOS_API_BASE_URL = 'https://jsonplaceholder.typicode.com'
+TODOS_TO_FETCH = 20
+
+# we are not using this method
+def stream_fetch
+  io = URI.open("#{TODOS_API_BASE_URL}/todos")
+  Yajl::Parser.parse(io) do |todos|
+    todos.select{ |todo| todo['id'] % 2 == 0 && todo['id'] <= 40 }.each{ |todo| yield todo }
+  end
+end
+
+def http_fetch
+  uri = URI(TODOS_API_BASE_URL)
+  Net::HTTP.start(uri.hostname) do |http|
+    (1..(TODOS_TO_FETCH * 2)).select(&:even?).each do |todo_index|
+      yield JSON.parse(http.get("/todos/#{todo_index}").body)
+    end
+  end
+end
+
+def main
+  table_config = [
+    { key: :title, title: 'Title', size: '*' },
+    { key: :completed, title: 'Completed', size: 12, justify: :left }
+  ]
+  ConsoleTable.define(table_config) do |table|
+    http_fetch { |todo| table << { title: todo['title'], completed: todo['completed'] ? ' Yes'.green : ' No'.red} }
+  end
+end
+
+# ensures that the main method is only called when the script is executed directly
+main if __FILE__ == $PROGRAM_NAME
